222a7e8db8577d6617b9500aa7c92f89cf58d9d7
PrajwalPatil1105 <2kl20cs120@klescet.ac.in>
2024-09-26 14:00:04 +0530

    Expanding Cards

---
+++ script.js
@@ -0,0 +1,164 @@
+            let id1=document.getElementById("i1")
+            let id2=document.getElementById("i2")
+            let id3=document.getElementById("i3")
+            let id4=document.getElementById("i4")
+            let id5=document.getElementById("i5")
+            let i1=document.getElementById("img1")
+            let i2=document.getElementById("img2")
+            let i3=document.getElementById("img3")
+            let i4=document.getElementById("img4")
+            let i5=document.getElementById("img5")
+            let F1=document.getElementById("font1")
+            let F2=document.getElementById("font2")
+            let F3=document.getElementById("font3")
+            let F4=document.getElementById("font4")
+            let F5=document.getElementById("font5")
+
+            function img1(){
+                id1.style.width="800px";
+                id1.style.transitionDuration="1s";
+                i1.style.width="800px";
+                i1.style.transitionDuration="1s";
+                setTimeout(() => {
+                    F1.innerText="The Big Bridge";
+                }, 900);
+                
+                F2.innerText="";
+                id2.style.width="100px";
+                id2.style.transitionDuration="1s";
+                i2.style.width="100px";
+                i2.style.transitionDuration="1s";
+                F3.innerText="";
+                id3.style.width="100px";
+                id3.style.transitionDuration="1s";
+                i3.style.width="100px";
+                i3.style.transitionDuration="1s";
+                F4.innerText="";
+                id4.style.width="100px";
+                id4.style.transitionDuration="1s";
+                i4.style.width="100px";
+                i4.style.transitionDuration="1s";
+                F5.innerText="";
+                id5.style.width="100px";
+                id5.style.transitionDuration="1s";   
+                i5.style.width="100px";
+                i5.style.transitionDuration="1s";   
+            }
+            function img2(){
+                id2.style.width="800px"
+                id2.style.transitionDuration="1s";
+                i2.style.width="800px";
+                i2.style.transitionDuration="1s"
+                setTimeout(() => {
+                    F2.innerText="The Mountains" 
+                }, 900);
+                F1.innerText=""
+                id1.style.width="100px";
+                id1.style.transitionDuration="1s";
+                i1.style.width="100px";
+                i1.style.transitionDuration="1s";
+                F3.innerText=""
+                id3.style.width="100px";
+                id3.style.transitionDuration="1s";
+                i3.style.width="100px";
+                i3.style.transitionDuration="1s";
+                F4.innerText="";
+                id4.style.width="100px";
+                id4.style.transitionDuration="1s";
+                i4.style.width="100px";
+                i4.style.transitionDuration="1s";
+                F5.innerText="";
+                id5.style.width="100px";
+                id5.style.transitionDuration="1s";   
+                i5.style.width="100px";
+                i5.style.transitionDuration="1s";   
+            }
+            function img3(){
+                id3.style.width="800px"
+                id3.style.transitionDuration="1s";
+                i3.style.width="800px"
+                i3.style.transitionDuration="1s";
+                setTimeout(() => { 
+                    F3.innerText="New York City "
+                }, 900);
+                F2.innerText=""
+                id2.style.width="100px";
+                id2.style.transitionDuration="1s";
+                i2.style.width="100px";
+                i2.style.transitionDuration="1s";
+                F1.innerText=""
+                id1.style.width="100px";
+                id1.style.transitionDuration="1s";
+                i1.style.width="100px";
+                i1.style.transitionDuration="1s";
+                F4.innerText="";
+                id4.style.width="100px";
+                id4.style.transitionDuration="1s";
+                i4.style.width="100px";
+                i4.style.transitionDuration="1s";
+                F5.innerText="";
+                id5.style.width="100px";
+                id5.style.transitionDuration="1s";   
+                i5.style.width="100px";
+                i5.style.transitionDuration="1s";    
+            }
+
+            function img4(){
+                id4.style.width="800px"
+                id4.style.transitionDuration="1s";
+                i4.style.width="800px"
+                i4.style.transitionDuration="1s";
+                setTimeout(() => { 
+                    F4.innerText="The Sea";
+                }, 900);
+                F2.innerText=""
+                id2.style.width="100px";
+                id2.style.transitionDuration="1s";
+                i2.style.width="100px";
+                i2.style.transitionDuration="1s";
+                F3.innerText=""
+                id3.style.width="100px";
+                id3.style.transitionDuration="1s";
+                i3.style.width="100px";
+                i3.style.transitionDuration="1s";
+                F1.innerText=""
+                id1.style.width="100px";
+                id1.style.transitionDuration="1s";
+                i1.style.width="100px";
+                i1.style.transitionDuration="1s";
+                F5.innerText="";
+                id5.style.width="100px";
+                id5.style.transitionDuration="1s";   
+                i5.style.width="100px";
+                i5.style.transitionDuration="1s";   
+            }
+
+            function img5(){
+                id5.style.width="800px"
+                id5.style.transitionDuration="1s";
+                i5.style.width="800px"
+                i5.style.transitionDuration="1s";
+                setTimeout(() => {
+                    F5.innerText="The Forest"
+                }, 900);
+                F2.innerText=""
+                id2.style.width="100px";
+                id2.style.transitionDuration="1s";
+                i2.style.width="100px";
+                i2.style.transitionDuration="1s";
+                F3.innerText=""
+                id3.style.width="100px";
+                id3.style.transitionDuration="1s";
+                i3.style.width="100px";
+                i3.style.transitionDuration="1s";
+                F4.innerText="";
+                id4.style.width="100px";
+                id4.style.transitionDuration="1s";
+                i4.style.width="100px";
+                i4.style.transitionDuration="1s";
+                F1.innerText=""
+                id1.style.width="100px";
+                id1.style.transitionDuration="1s";   
+                i1.style.width="100px";
+                i1.style.transitionDuration="1s";   
+            }
